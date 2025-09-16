3a09894a2795f40a2802fa6c08441d19036c7c3e
iutgeiitoulonE105 <touloniut@gmail.com>
2025-09-16 11:29:31 +0200

    adc

---
+++ Robot_Choukrani_bilal_Capre_noah.X/ADC.h
@@ -0,0 +1,11 @@
+
+
+#ifndef ADC_H
+#define	ADC_H
+void InitADC1(void);
+void __attribute__((interrupt, no_auto_psv)) _AD1Interrupt(void);
+void ADC1StartConversionSequence();
+
+
+#endif	/* ADC_H */
+
