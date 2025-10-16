24d5d0b3b507114efe6ab51d9be7bfbd0f25d861
E304-PC7 <touloniut@gmail.com>
2025-10-16 12:26:56 +0200

    vs code

---
+++ C#/Robotinterface_Chaukrani_Capre/Robotinterface_Chaukrani_Capre/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Robotinterface_Chaukrani_Capre
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ C#/Robotinterface_Chaukrani_Capre/Robotinterface_Chaukrani_Capre/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Robotinterface_Chaukrani_Capre
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool toggle;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if(toggle == !toggle)
+            {
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+            }
+            else
+            {
+                buttonEnvoyer.Background = Brushes.Beige;
+            }
+        }
+    }
+}
